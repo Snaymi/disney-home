d85fa92ac4ac86a85b8388970a318414374ac102
Snaymi <98271156+Snaymi@users.noreply.github.com>
2025-10-29 10:21:35 -0300

    Starting the project

---
+++ disney-home/src/App.vue
@@ -0,0 +1,14 @@
+<script setup lang="ts">
+</script>
+
+<template>
+  <div class="min-h-screen flex items-center justify-center bg-gray-50">
+    <h1 class="text-3xl font-bold underline">
+      Hello world!
+    </h1>
+  </div>
+</template>
+
+<style>
+
+</style>
